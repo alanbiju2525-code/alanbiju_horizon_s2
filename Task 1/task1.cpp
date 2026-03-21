23a6dc5e27e368d932550d8f24c74e3c4479c8bc
alanbiju2525-code <alanbiju2525@gmail.com>
2026-03-21 19:34:29 +0530

    Task 1 part B

--- Task 1/task1.cpp
+++ Task 1/task1.cpp
@@ -5,8 +5,12 @@ using namespace std;
 class Rover {
 private:
     double x1, y1, x2, y2;
+    double initialVelocity, acceleration, maxSpeed;
+    double distance;
 
 public:
+
+    // Input coordinates
     void getCoordinates() {
         cout << "Enter origin (x1 y1): ";
         cin >> x1 >> y1;
@@ -15,13 +19,64 @@ public:
         cin >> x2 >> y2;
     }
 
-    double calculateDistance() {
-        double dist = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-        return dist;
+    // Input motion parameters
+    void getMotionParameters() {
+        cout << "Enter initial velocity: ";
+        cin >> initialVelocity;
+
+        cout << "Enter acceleration: ";
+        cin >> acceleration;
+
+        cout << "Enter maximum speed: ";
+        cin >> maxSpeed;
     }
 
-    void displayDistance(double dist) {
-        cout << "Distance to destination: " << dist << " meters" << endl;
+    // Calculate distance
+    void calculateDistance() {
+        distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    }
+
+    // Calculate time
+    double calculateTime() {
+
+        double timeToMax = (maxSpeed - initialVelocity) / acceleration;
+
+        if (timeToMax < 0)
+            timeToMax = 0;
+
+        // Distance covered during acceleration
+        double distAccel = (initialVelocity * timeToMax) +
+                           (0.5 * acceleration * pow(timeToMax, 2));
+
+        double time = 0;
+
+        if (distAccel >= distance) {
+            // Rover doesn't reach max speed
+
+            double a = 0.5 * acceleration;
+            double b = initialVelocity;
+            double c = -distance;
+
+            double D = b * b - 4 * a * c;
+
+            time = (-b + sqrt(D)) / (2 * a);
+
+        } else {
+            // Rover reaches max speed, then moves at constant speed
+
+            double remainingDist = distance - distAccel;
+            double timeConst = remainingDist / maxSpeed;
+
+            time = timeToMax + timeConst;
+        }
+
+        return time;
+    }
+
+    // Display result
+    void display(double time) {
+        cout << "\nDistance to destination: " << distance << " meters" << endl;
+        cout << "Time required: " << time << " seconds" << endl;
     }
 };
 
@@ -29,10 +84,13 @@ int main() {
     Rover r;
 
     r.getCoordinates();
+    r.calculateDistance();
 
-    double distance = r.calculateDistance();
+    r.getMotionParameters();
 
-    r.displayDistance(distance);
+    double time = r.calculateTime();
+
+    r.display(time);
 
     return 0;
 }
